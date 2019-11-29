c6b52326c082066e0a49cc1f123d4258f1f99e3b
frivolta <rivoltafilippo@gmail.com>
2019-11-29 22:30:30 +0100

    :building_construction: Changes app layout skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-import React, { useState } from 'react';
-import { Column, Row } from 'simple-flexbox';
-import { StyleSheet, css } from 'aphrodite';
+import React from 'react';
 
-import { Sidebar } from './components/layout/sidebar/Sidebar';
-import { Header } from './components/layout/header/Header';
 import { IApp } from './interfaces';
-
-const styles = StyleSheet.create({
-  container: {
-    height: '100vh'
-  },
-  content: {
-    marginTop: 54
-  },
-  mainBlock: {
-    backgroundColor: '#F7F8FC',
-    padding: 30
-  }
-});
+import { Sidebar } from './components/layout/sidebar/Sidebar';
+import { Content } from './components/layout/content/Content';
+import { Header } from './components/layout/header/Header';
 
 //@Todo: Should be a custom hooks, handled with context
 //@Todo: Responsive version
@@ -27,19 +13,11 @@ const styles = StyleSheet.create({
 //@Todo: PWA version
 
 export const App: React.FC<IApp> = () => {
-  const [selectedItem, setSelectedItem] = useState('Projects');
-
-  const handleSelectedItem = (newItem: string) => setSelectedItem(newItem);
-
   return (
-    <Row className={css(styles.container)}>
-      <Sidebar selectedItem={selectedItem} onChange={handleSelectedItem} />
-      <Column flexGrow={1} className={css(styles.mainBlock)}>
-        <Header title={selectedItem} />
-        <div className={css(styles.content)}>
-          <span>Content</span>
-        </div>
-      </Column>
-    </Row>
+    <>
+      <Header />
+      <Sidebar />
+      <Content />
+    </>
   );
 };
---
+++ src/components/layout/content/Content.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Content: React.FC = () => (
+  <div className="Content">
+    <div className="Content__title">
+      <h1>Content</h1>
+    </div>
+    <div className="Content__main">
+      <div className="Card">card component</div>
+    </div>
+  </div>
+);
