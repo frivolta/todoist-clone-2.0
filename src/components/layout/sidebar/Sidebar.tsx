f5e71b4fe51910fd250a57f2cc2fe06e41279886
frivolta <rivoltafilippo@gmail.com>
2019-11-30 12:20:43 +0100

    :lipstick: Stilyng sidebar

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -5,8 +5,77 @@ import React from 'react';
 export const Sidebar: React.FC = props => {
   return (
     <div className="Sidebar Sidebar--open">
-      <h1>Sidebar</h1>
-      <button className="toggle-btn">toggle</button>
+      <ul className="Menu">
+        <li className="Menu__item">
+          <a className="Menu__item__link" href="/">
+            Inbox
+          </a>
+        </li>
+        <li className="Menu__item">
+          <a className="Menu__item__link" href="/">
+            Next 7 days
+          </a>
+        </li>
+        <li className="Menu__item">
+          <a className="Menu__item__link" href="/">
+            Today
+          </a>
+        </li>
+      </ul>
+
+      <hr />
+
+      <span className="Title Title--secondary">My list</span>
+      <div className="ProjectList">
+        <div className="ProjectList__item">
+          <span
+            className="ProjectList__item__label ProjectList__item__label--active"
+            role="button"
+            onClick={() => console.log('item clicked')}
+          >
+            Grocery store
+          </span>
+          <span
+            className="ProjectList__item__button ProjectList__item__button--active"
+            role="button"
+            onClick={() => console.log('item deleted')}
+          >
+            <img src="/images/delete.svg" alt="delete button" />
+          </span>
+        </div>
+        <div className="ProjectList__item">
+          <span
+            className="ProjectList__item__label"
+            role="button"
+            onClick={() => console.log('item clicked')}
+          >
+            Christmas gifts
+          </span>
+          <span
+            className="ProjectList__item__button"
+            role="button"
+            onClick={() => console.log('item deleted')}
+          >
+            <img src="/images/delete.svg" alt="delete button" />
+          </span>
+        </div>
+        <div className="ProjectList__item">
+          <span
+            className="ProjectList__item__label"
+            role="button"
+            onClick={() => console.log('item clicked')}
+          >
+            My favourite places
+          </span>
+          <span
+            className="ProjectList__item__button"
+            role="button"
+            onClick={() => console.log('item deleted')}
+          >
+            <img src="/images/delete.svg" alt="delete button" />
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
